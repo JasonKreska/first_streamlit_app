4041f65a4500ddaeaab8321fc72e565806b86c73
JASON KRESKA <89426742+JasonKreska@users.noreply.github.com>
2023-02-23 07:27:46 -0500

    Update streamlit_app.py
    Tried commenting out changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner') 
 
-streamlit.header('Gruesome Grub')
+--streamlit.header('Gruesome Grub')
 
-streamlit.text('Eyeball Salad       4.99')
-streamlit.text('Chikn Fingers       5.99')
-streamlit.text('Broccoli Brains     5.99')
+--streamlit.text('Eyeball Salad       4.99')
+--streamlit.text('Chikn Fingers       5.99')
+--streamlit.text('Broccoli Brains     5.99')
