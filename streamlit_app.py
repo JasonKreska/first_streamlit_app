6dc9db0669e94020e17f90947f6ddd9ba9607440
JASON KRESKA <89426742+JasonKreska@users.noreply.github.com>
2023-02-23 07:43:28 -0500

    Update streamlit_app.py
    re-update emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text(':ramen: Omega 3 & Blueberry Oatmeal')
-streamlit.text(':salad: Kale, Spinach & Rocket Smoothie')
-streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+streamlit.text('🍜 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🌿 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
