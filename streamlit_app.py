6ef697073cb855c41b17a0a4f056b77337f7ccd5
JASON KRESKA <89426742+JasonKreska@users.noreply.github.com>
2023-02-23 07:24:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner') 
 
 streamlit.header('Gruesome Grub')
+
+streamlit.text('Eyeball Salad       4.99')
+streamlit.text('Chikn Fingers       5.99')
+streamlit.text('Broccoli Brains     5.99')
